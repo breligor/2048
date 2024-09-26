425f11a1fdda30d33b8881d75cef0d504c7a8fe5
User <brelig@yandex.ru>
2024-09-26 17:50:51 +0300

    initial commit

---
+++ src/utils.js
@@ -0,0 +1,45 @@
+export function drawRoundedRect(ctx, x, y, width, height, radius) {
+    ctx.beginPath();
+    ctx.moveTo(x + radius, y);
+    ctx.lineTo(x + width - radius, y);
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+    ctx.lineTo(x + width, y + height - radius);
+    ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+    ctx.lineTo(x + radius, y + height);
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+    ctx.lineTo(x, y + radius);
+    ctx.quadraticCurveTo(x, y, x + radius, y);
+    ctx.closePath();
+}
+
+export function getTileColor(value) {
+    const tileColors = {
+        2: '#eee4d9',
+        4: '#ede0c7',
+        8: '#f9b376',
+        16: '#ff9b60',
+        32: '#ca6a49',
+        64: '#ec6233',
+        128: '#e8c562',
+        256: '#dfba55',
+        512: '#f3c54b',
+        1024: '#f2c138',
+        2048: '#f2bd28',
+    };
+
+    return tileColors[value] || '#cfc0af';
+}
+
+export function updateScore(score) {
+    const scoreField = document.querySelector('#score');
+
+    scoreField.innerHTML = `Счет: ${score}`;
+}
+
+export function getScoreText(score) {
+    if (score === 32 || score === 64) {
+        return `очка`;
+    }
+    
+return `очков`;
+}
